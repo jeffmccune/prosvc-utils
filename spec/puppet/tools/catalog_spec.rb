c1e67b8ded868144564a7191f8a5a38f30d2bb5a
Dan Bode <bodepd@gmail.com>
2011-01-31 12:02:41 -0600

    added unit tests for catalog tools code.

--- spec/puppet/tools/catalog_spec.rb
+++ spec/puppet/tools/catalog_spec.rb
@@ -1,6 +1,7 @@
 require 'puppet'
 require 'puppet/tools/catalog'
 require 'yaml'
+require 'puppet/external/pson/common'
 
 # find all of the puppet spec stuff (more than a little ghetto)
 def find_puppet_spec()
@@ -14,28 +15,68 @@ def find_puppet_spec()
 end
 find_puppet_spec
 require 'puppet_spec/files'
-include Puppet::Tools::Compile
+include Puppet::Tools::Catalog
 include PuppetSpec
 include PuppetSpec::Files
+include PSON
 
-describe Puppet::Tools::Catalog do
-  def mkscope()
-
-  end
+describe 'Puppet::Tools::Catalog' do
   def mkresource(type, name)
-    Puppet::Parser::Resource.new(type, name, :source => @source, :scope => @scope)
+    Puppet::Parser::Resource.new(type, name, :source => nil, :scope => @scope)
   end
-  # load the helper libs from puppet spec
-  # done setting up path
-  # I dont know how to stub this,
-  # should I create fakes dirs or what?
+  def code_to_catalog(code, node_name, facts, classes={}) 
+    node = Puppet::Node.new(node_name, :classes => classes)
+    node.merge(facts)
+    Puppet[:code]=code
+    compiled_catalog = Puppet::Resource::Catalog.find(node.name, :use_node=>node)
+  end
+  def print_catalog(compiled_catalog)
+    puts PSON::pretty_generate(
+      compiled_catalog,
+      :allow_nan => true,
+      :max_nesting => false
+    ).to_s
+  end
+  # generating puppet code is the best way to test this
   before :each do 
+    @node = Puppet::Node.new("mynode")
+    @compiler = Puppet::Parser::Compiler.new(@node)
+    @scope = @compiler.topscope
     @catalog = Puppet::Resource::Catalog.new("host")
+    @code_foo_class = 'class foo{notify{bar: message => bo}}'
+    @code_include_foo = 'include foo'
+    @code_baz_class = "class baz{Notify[baz]->Notify[bar]\nnotify{baz:}}\ninclude baz"
+    @code_default_node = 'node default{}'
+    @code_define_bar = 'define bar() {notify{$name:}}'
+    @code_declare_bar = 'bar{name:}'
+    @code = "#{@code_foo_class}\n#{@code_include_foo}\n#{@code_default_node}"
+    @code << "\n#{@code_define_bar}"
+    @code << "\n#{@code_declare_bar}"
+    @code << "\n#{@code_baz_class}"
+    @catalog =  code_to_catalog(@code, 'foonode', {'one'=>'1'})
   end
 
-
-  describe 'foo' do
-    
+  describe 'when getting a catlogs resources' do
+    it 'should not return containers' do
+      resources = get_resources(@catalog) 
+puts resources.inspect
+      resources[["Notify", "name"]][:name].should == "name"
+      resources[["Notify", "bar"]][:name].should == "bar"
+      resources[["Node", "default"]].should be_nil
+      resources[["Bar", "name"]].should be_nil
+    end
+    it 'should return containers when :show_containers => true' do
+      resources = get_resources(@catalog) 
+      code = "#{@code_foo_class}\n#{@code_include_foo}\n#{@code_default_node}"
+      code << "\n#{@code_define_bar}"
+      code << "\n#{@code_declare_bar}"
+      catalog =  code_to_catalog(code, 'foonode', {'one'=>'1'})
+      resources = get_resources(catalog, :show_containers => true) 
+      resources[["Notify", "name"]][:name].should == "name"
+      resources[["Notify", "bar"]][:name].should == "bar"
+      resources[["Class", "Foo"]].should_not be_nil
+      resources[["Node", "default"]].should_not be_nil
+      resources[["Bar", "name"]][:name].should == 'name'
+    end
   end
-
 end
