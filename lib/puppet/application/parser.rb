4e005ff101c714f7eb64fb6760a5f1258c316a9b
Dan Bode <bodepd@gmail.com>
2011-02-01 03:36:33 -0600

    added ability to write out yaml files per parsed node.

--- lib/puppet/application/parser.rb
+++ lib/puppet/application/parser.rb
@@ -17,6 +17,7 @@ class Puppet::Application::Parser < Puppet::Application
       # set log levels
       :verbose => false,
       :debug => false,
+      :outputdir => "#{Puppet[:vardir]}/nodes/yaml"
     }.each do |opt, value|
       options[opt]=value
     end
@@ -29,9 +30,14 @@ class Puppet::Application::Parser < Puppet::Application
   option('--file FILENAME') do |args|
     options[:file] = args
   end
+  option('--outputdir DIR') do |args|
+    options[:outputdir] = args
+  end
   option('--get_nodes')
+  option('--create_yaml')
 
   def setup 
+    FileUtils.mkdir_p(options[:outputdir]) unless File.exists?(options[:outputdir])
     raise Puppet::Error, 'must specify --file FILENAME to parse' unless options[:file]
   end
 
@@ -40,7 +46,16 @@ class Puppet::Application::Parser < Puppet::Application
     @text = File.read(options[:file])
     if options[:get_nodes] 
       @parser.code=@text
-      puts @parser.parse_nodes.keys
+      # this will not work well with regex
+      nodes = @parser.parse_nodes.keys
+      puts "Found the following nodes"
+      nodes.each do |node|
+        puts node
+        filename = File.join(options[:outputdir], "#{node}.yaml")
+        puts "Writing file: #{filename}"
+        puts "#{filename} already exists" if File.exists?(filename)
+        FileUtils.touch(filename)
+      end
     else
       raise Puppet::Error, 'can only get nodes, must pass --get_nodes'
     end
